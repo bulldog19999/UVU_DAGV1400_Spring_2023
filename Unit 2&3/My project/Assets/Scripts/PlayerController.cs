2b92f8a09a2b30660764a7b91c95913c368c0c38
Michael Brumwell <brumwellm1@gmail.com>
2024-02-17 19:30:43 -0700

    Unity tutorials
    Created PLayer Controller class and updated to take input with W,A,S,D keys. Currently lack movement functionality.
    
    Created color changing script and folder for unity tutorials.

---
+++ Unit 2&3/My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("W");
+        }   
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("S");
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("D");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump");
+        }
+    }
+}
