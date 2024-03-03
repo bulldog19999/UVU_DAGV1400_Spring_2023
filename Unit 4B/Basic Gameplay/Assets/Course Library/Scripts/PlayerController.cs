52afd501ddbdb6f94599d7a86824473c0b127fcf
Michael Brumwell <brumwellm1@gmail.com>
2024-03-02 20:50:25 -0700

    Unit 4B
    Started Basic Gameplay project and completed up to Lesson 2.2 - 5th lesson

---
+++ Unit 4B/Basic Gameplay/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+//TODO: Continue from 2.2.6
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed;
+    public float rangeLimit;
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        speed = 10f;
+        rangeLimit = 10f;
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        //Limit player movement, although I think I'd rather have an invisible wall than code
+        if(transform.position.x < -rangeLimit)
+        {
+            transform.position = new Vector3(-rangeLimit, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > rangeLimit)
+        {
+            transform.position = new Vector3(rangeLimit, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch Projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+    }
+}
