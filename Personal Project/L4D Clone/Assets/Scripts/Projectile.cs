3f30c9ec00bcd60af89ca14ff7c8c98e5908610e
Michael Brumwell <brumwellm1@gmail.com>
2024-03-24 19:54:28 -0700

    Updated Personal Project
    -Fixed Collision with goal by changing collision event to trigger
    -Added win/lose conditions
    -Restricted player movement and abilities to gameOver bool
    -Added shooting and sprinting functionality
    -Added projectile code and it's own collision functionality

---
+++ Personal Project/L4D Clone/Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody projectileRb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectileRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        projectileRb.AddForce(Vector3.forward * 3.0f, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")) 
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    
+
+}
